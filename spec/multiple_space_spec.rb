7b3564095cc6f37658fa8c484c382990cd78450e
shubham14p3 <shubham14p3@gmail.com>
2020-06-24 02:31:57 +0530

    add stickler and fixed offences

--- spec/multiple_space_spec.rb
+++ spec/multiple_space_spec.rb
@@ -5,21 +5,20 @@ include MultipleSpace
 # include Colorize
 
 describe MultipleSpace do
-    let(:file) { './style.css' }
-    let(:error_array) { [] }
-    let(:file_details) {File.readlines('./style.css' )}
-  
-  describe '#multiple_space_check' do
-  
-    it 'returns an empty array when all the colons have the right afterspace' do
-    #   subject.multiple_space_check(file_details[2], 2, error_array)
+  let(:file) { './style.css' }
+  let(:error_array) { [] }
+  let(:file_details) { File.readlines('./style.css') }
 
-    multiple_space_check(file_details[34], 34, error_array)
+  describe '#multiple_space_check' do
+    it 'returns an empty array when all the colons have the right afterspace' do
+      #   subject.multiple_space_check(file_details[2], 2, error_array)
+
+      multiple_space_check(file_details[34], 34, error_array)
       expect(error_array).to be_a(Array)
-        # file_details.each_with_index { |line, index| after_colon_check(line_content, line_index, error_messages) }end.to be_a_kind_of(Array)
-        # expect(MultipleSpace.multiple_space_check(file_details[2], 2, error_array)).to return_a(Array)
+      # file_details.each_with_index { |line, index| after_colon_check(line_content, line_index, error_messages) }end.to be_a_kind_of(Array)
+      # expect(MultipleSpace.multiple_space_check(file_details[2], 2, error_array)).to return_a(Array)
     end
-end
+  end
 end
 
 # describe "sum method" do
